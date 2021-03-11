e2468a0dee3369a5083fe7d048af1bdae94e2a62
Carson Scholberg <cscholbe@gustavus.edu>
2021-03-11 16:26:37 -0600

    generates properly

--- index.js
+++ index.js
@@ -1,40 +1,81 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const teamGenerator = require("./src/page-template");
+
+let hasManager = false;
 
 const employeeArr = []
 function init() {
     inquirer.prompt([
         {
-            type: "input",
-            name: "name",
-            message: "What is your new Manager's name? "
-        },
-        {
-            name: "id",
-            message: "Enter their ID: "
-        },
-        {
-            name: "email",
-            message: "Enter their Email: "
-        },
-        {
-            name: "officeNumber",
-            message: "Enter their officeNumber: "
-        },
-    ]).then(res => {
-        const managerResponse = new Manager(res.name, res.id, res.email, res.officeNumber)
-        employeeArr.push(managerResponse);
-        mainMenu()
-        // fs.writeFile("index.html", teamGenerator(employeeArr), err => {
-        //     if(err) throw err;
-        // })
+            type: "list",
+            name: "roleChoice",
+            message: "Which Role are we Adding?",
+            choices: hasManager ? ["Engineer", "Intern", "Done"] : ["Manager", "Engineer", "Intern", "Done"]
+        }
+    ]).then(roleResponse => {
+        if (roleResponse.roleChoice != "Done") {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "name",
+                    message: "What is your new Employee's name? "
+                },
+                {
+                    name: "id",
+                    message: "Enter their ID: "
+                },
+                {
+                    name: "email",
+                    message: "Enter their Email: "
+                },
+            ]).then(basicInfo => {
+                if (roleResponse.roleChoice == "Manager") {
+                    inquirer.prompt([
+                        {
+                            name: "office",
+                            message: "Enter their Office Number:"
+                        }
+                    ]).then(managerRes => {
+                        const manager = new Manager(basicInfo.name, basicInfo.id, basicInfo.email, managerRes.office)
+                        employeeArr.push(manager)
+                        hasManager = true
+                        init()
+                    })
+                } else if (roleResponse.roleChoice == "Engineer") {
+                    inquirer.prompt([
+                        {
+                            name: "github",
+                            message: "Enter their Github Account:"
+                        }
+                    ]).then(engineerRes => {
+                        const engineer = new Engineer(basicInfo.name, basicInfo.id, basicInfo.email, engineerRes.github)
+                        employeeArr.push(engineer)
+                        init()
+                    })
+                } else if (roleResponse.roleChoice == "Intern") {
+                    inquirer.prompt([
+                        {
+                            name: "school",
+                            message: "Enter their School Name:"
+                        }
+                    ]).then(internRes => {
+                        const intern = new Intern(basicInfo.name, basicInfo.id, basicInfo.email, internRes.school)
+                        employeeArr.push(intern)
+                        init()
+                    })
+                }
+
+            })
+        } else {
+            fs.writeFile("index.html", teamGenerator(employeeArr), err => {
+                if (err) throw err;
+            })
+        }
     })
-}
-
-function mainMenu () {
-
 }
 
 init();
